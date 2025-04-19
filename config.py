1ab3a4d5929e55c039c19c8fbedc79fb10a15c45
quangnghia1110 <nqndev.work@gmail.com>
2025-04-19 19:58:39 +0700

    update

--- config.py
+++ config.py
@@ -23,3 +23,5 @@ MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+CHAT_URL = os.getenv("CHAT_URL")
